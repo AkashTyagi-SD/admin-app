0d5dd3e32b7f35c146414d26f00464ba1300bfc1
AkashTyagi-SD <akashtyagi245205@gmail.com>
2023-12-23 22:57:50 +0530

    commit login and register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,4 @@
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -13,7 +13,7 @@ const LoginPage = () => {
             placeholder="Username"
           />
         </div>
-        <div className="mb-6">
+        <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">
             Password
           </label>
@@ -24,23 +24,28 @@ const LoginPage = () => {
             placeholder="Enter Password"
           />
         </div>
-        <div className="flex items-center justify-between">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Confirm Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="confirm-password"
+            type="password"
+            placeholder="Enter Password"
+          />
+        </div>
+        <div className="flex items-center justify-center">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
           >
-            Sign In
+            Register
           </button>
-          <a
-            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-            href="#"
-          >
-            Forgot Password?
-          </a>
         </div>
       </form>
     </div>
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
