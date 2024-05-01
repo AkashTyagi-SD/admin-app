ca6b4f222550ea78d1590d5203317a19634d28ea
AkashTyagi-SD <akashtyagi245205@gmail.com>
2024-05-01 18:35:51 +0530

    commit some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello</p>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Welcome to Contact management app</h1>
+      <div className="w-2/5 flex justify-between">
+        <Link href="/login" className="hover:text-red-500">
+          Login
+        </Link>
+        <h2>Or</h2>
+        <Link href="/sign-up" className="hover:text-red-500">
+          Sign Up
+        </Link>
+      </div>
     </main>
   );
 }
